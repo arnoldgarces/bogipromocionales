bb4611a4fc7124d4cb5e230f6d6304c084073a6b
Arnold Garces <bombillomn94@hotmail.com>
2016-08-03 21:10:25 -0500

    Archivo configuracion

---
+++ Connections/local.php
@@ -0,0 +1,8 @@
+<?php
+
+$hostname_conexion = "localhost";
+$database_conexion = "bogi";
+$username_conexion = "root";
+$password_conexion = "";
+
+?>
---
+++ Connections/production.php
@@ -0,0 +1,8 @@
+<?php
+
+$hostname_conexion = "mysql.hostinger.co";
+$database_conexion = "u254524781_bogi";
+$username_conexion = "u254524781_bogi";
+$password_conexion = "valentinaydaniel";
+
+?>
