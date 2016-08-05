6cf922559a8746ed57263cc4d1f90894f079778e
Arnold Garces <bombillomn94@hotmail.com>
2016-08-05 13:22:59 -0500

    mensaje

--- sendbymail.php
+++ sendbymail.php
@@ -7,7 +7,6 @@ $email_subject = "Hey!, tenemos una nueva inquietud";
 
 // Aquí se deberían validar los datos ingresados por el usuario
 if(!isset($_POST['first_name']) ||
-!isset($_POST['last_name']) ||
 !isset($_POST['email']) ||
 !isset($_POST['telephone']) ||
 !isset($_POST['comments'])) {
